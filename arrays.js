83f89b85eccb2f482a0f5f57aadb4619b1639013
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-03-29 23:02:29 +0300

    changed file name

---
+++ arrays.js
@@ -0,0 +1,59 @@
+// Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+
+function reversedStrings(arr, indices) {
+    arr = arr.map(str => str.split('').reverse().join(''));
+    indices.forEach(a => { if (a < arr.length) { arr[a] = arr[a].split('').reverse().join(''); } });
+    return arr.sort();
+}
+const strings = ["apple", "banana", "orange", "grape", "Tomato", "Pineaple"]; const indicesToReverse = [0, 4];
+const reversed = reversedStrings(strings, indicesToReverse);
+console.log(reversed);
+
+
+// Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+
+function checkPositiveNegative(arr) {
+    if (arr.some(num => num > 0)) { return "positive"; }
+    else if (arr.some(num => num < 0)) {
+        return "negative";
+    } else { return "Zero"; }
+}
+const arrNumbers = [0, 0, 0, 0];
+console.log(checkPositiveNegative(arrNumbers));
+
+
+// Given an array of objects, where each object represents an employee with an id, name, and salary property, write a function that returns a new array of employee objects sorted by their salary in ascending order.
+
+function sortedBySalary(employee) {
+    return employee.sort((x, y) => x.salary - y.salary);
+}
+const employee = [{ id: 1, name: "Luke", salary: 1000000 },
+{ id: 2, name: "Diana", salary: 700000 },
+{ id: 3, name: "Brian", salary: 130000 }];
+const sortedArr = sortedBySalary(employee);
+console.log(sortedArr);
+
+
+// Write a function that accepts an array of numbers and uses the forEach() method to console.log each number multiplied by 2.
+
+
+const arrNums = [10,11,22,30,31];
+let numsMultipliedTwo =[];
+arrNums.forEach(function(num){
+    numsMultipliedTwo.push(num*2);
+})
+console.log({numsMultipliedTwo});
+
+
+// Write a function that takes in an array of numbers and consoles the first four items multiplied by 8 and the last two added by 5. Console the array with the new values
+
+function multiply(arr,n) {
+    var product = 8;
+    n = arr.length;
+    for (var i = 0; 1 < 3; i++) {
+        product *= arr[i];
+    }
+    console.log(product);
+
+}
+multiply([1,2,3,4,],4)
